75137a238e2df1061a9d1e24bbc63e8e1dbebf54
ynskrca12 <74262367+ynskrca12@users.noreply.github.com>
2021-03-09 14:21:06 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        IRuleService<IEntity> _rules;
+       
+        public CarManager(ICarDal carDal, IRuleService<IEntity> ruleService)
+        {
+            _carDal = carDal;
+            _rules = ruleService;
+
+        }
+
+
+        public void Add(Car entity)
+        {
+            _rules.MinNameRule(entity);
+            _rules.MinPriceRule(entity);
+            _carDal.Add(entity);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+      
+       
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Abstract;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IRuleService<IEntity> rulesService = new RuleManager();
+            
+
+            Car car1 = new Car
+            {
+                BrandId = 2,
+                ColorId = 2,
+                DailyPrice = 100,
+                ModelYear = "2017",
+                Description = "Tesla"
+            };
+            
+            ICarService carManager = new CarManager(new EfCarDal(), new RuleManager());
+
+            carManager.Add(car1);
+            
+            foreach (var car in carManager.GetAll())
+            {
+               
+                Console.WriteLine(car.Description);
+            }
+
+        }
+    }
+}
